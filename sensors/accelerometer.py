501581f6f6bfec7231e8896d86e173a409013772
Vijayaragavan S <144512525+vijay-saravanan@users.noreply.github.com>
2025-10-02 16:20:54 +0530

    Create accelerometer.py

---
+++ sensors/accelerometer.py
@@ -0,0 +1,5 @@
+import numpy as np
+class AccelerometerSensor:
+    def read(self):
+        # Replace with real accelerometer reading
+        return np.random.randn(128), np.random.randn(128), np.random.randn(128)
