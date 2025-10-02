5ebea28c579da980387cd66c6abcfd32e83c74cc
Vijayaragavan S <144512525+vijay-saravanan@users.noreply.github.com>
2025-10-02 16:22:35 +0530

    Create led.py

---
+++ ui/led.py
@@ -0,0 +1,7 @@
+class LED:
+    def __init__(self, pin1, pin2, pin3):
+        pass
+    def on(self, led_num):
+        print(f"LED {led_num} ON")
+    def all_off(self):
+        print("All LEDs OFF")
